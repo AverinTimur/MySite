921c846ac2654a2c583e859c4a164a24f8af5626
Timur <averintimur123@gmail.com>
2022-02-11 22:52:56 +0500

    jQuery 1.0

--- static/JS/Main.js
+++ static/JS/Main.js
@@ -4,19 +4,19 @@ function OpenMenu(){
     $("#Menu").css("display","inline");
     $("#Menu").animate({left: 0},300,"linear");
     $("#MenuOff").css("display","inline")
-    $("#Menu-Open-Text").animate({opacity: 0},500,"linear");
+    $("#Menu-Open-Text").animate({opacity: 0},300,"linear");
     setTimeout(function (){
         $("#Menu-Open-Text").css("display","none");
-    },500)
+    },300)
 }
 function CloseMenu(){
     $("#MenuOff").css("display","none");
     $("#Menu").animate({left: "-6cm"},300,"linear");
-    $("#Menu-Open-Text").animate({opacity: 1},500,"linear");
+    $("#Menu-Open-Text").animate({opacity: 1},300,"linear");
     setTimeout(function (){
         $("#Menu-Open-Text").css("display","block");
         $("#Menu").css("display","none");
-    },400)
+    },300)
 }
 function AnimationHover(id){
     $("#" + id).attr("class","onWork");
